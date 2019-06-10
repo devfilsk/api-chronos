4bad6874064c467fa40914de82ac5f9b874800ed
Filipe Maciel <devfilsk@gmail.com>
2019-06-10 07:17:32 -0300

    Configurando login com JWTTonek

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -36,4 +37,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        //informações para serem adicionadas no payload do jwtToken
+        return [
+            'name' => $this->name,
+            'email' => $this->email
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:api'], function (){
+    Route::get('users', function (){
+        return \App\User::all();
+    });
+
+//    Route::resource('cliente', 'ClienteController', ['except' => ['create', 'edit']]);
+
+});
+
+Route::name('api.login')->post('login', 'Api\AuthController@login');
